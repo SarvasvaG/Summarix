e3696302535b04d60e77843b95da50320d0122c3
SarvasvaG <sarvasva_g@cs.iitr.ac.in>
2023-10-31 18:55:20 +0530

    Release of Version 1.1
    1. Add a contact form.
    2. Change the background images.
    3. Add contact us section in navigation bar.
    4. Complete all the .html and .css files.

--- summariser.py
+++ summariser.py
@@ -1,5 +1,13 @@
-import nltk 
+import nltk
+import ssl
 from nltk.tokenize import word_tokenize, sent_tokenize 
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 nltk.download('stopwords')
 nltk.download('punkt')
