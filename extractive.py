94e37236177ba929f8444234b2e098ec923c2bb9
Anvit Gupta <142968120+anvitgupta011@users.noreply.github.com>
2023-11-05 20:32:54 +0530

    Add ssl to extractive.py

--- extractive.py
+++ extractive.py
@@ -1,7 +1,15 @@
 import re
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
+import ssl
 
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+    
 nltk.download('stopwords')
 nltk.download('punkt')
 from nltk.corpus import stopwords
